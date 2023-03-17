475e2d6bc02ea90e99b38705c8c1b31076a229b6
Josie <jandersoj03@gmail.com>
2023-03-17 00:41:37 -0400

    practice quiz 2

---
+++ lectures/lecture16/01-warmup/index.js
@@ -0,0 +1,27 @@
+function toggleFall() {
+    /*
+        Your task: target the body element and change its class to "fall"
+    */
+   document.querySelector("body").className = 'fall';
+}
+
+function toggleWinter() {
+    /*
+        Your task: target the body element and change its class to "winter"
+    */
+        document.querySelector("body").className = 'winter';
+}
+
+function toggleSpring() {
+    /*
+        Your task: target the body element and change its class to "spring"
+    */
+        document.querySelector("body").className = 'spring';
+}
+
+function toggleUnca() {
+    /*
+        Your task: target the body element and change its class to "spring"
+    */
+        document.querySelector("body").className = 'unca';
+}
